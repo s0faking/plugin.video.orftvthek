59683cc1399fffa53f3d1e5fe43bed616be7d2a8
s0faking <sofaking@gettingmoney.at>
2019-11-06 21:05:56 +0100

    first steps towards python3 support

--- resources/lib/helpers.py
+++ resources/lib/helpers.py
@@ -3,6 +3,40 @@
 
 import xbmc
 import xbmcaddon
+import sys
+
+try:
+    from urllib.parse import unquote, urlencode
+    from urllib.request import urlopen as OpenRequest
+    from urllib.request import Request as HTTPRequest
+    from urllib.error import HTTPError
+except ImportError:
+    from urllib import unquote, urlencode
+    from urllib2 import HTTPError
+    from urllib2 import urlopen as OpenRequest
+    from urllib2 import Request as HTTPRequest
+
+
+def unqoute_url(url):
+    return unquote(url)
+
+
+def build_kodi_url(parameters):
+    return sys.argv[0] + '?' + encode_parameters(parameters)
+
+
+def encode_parameters(parameters):
+    return urlencode(parameters)
+
+
+def url_get_request(url, authorization=False):
+    if authorization:
+        request = HTTPRequest(url)
+        request.add_header('Authorization', 'Basic %s' % authorization)
+    else:
+        request = url
+    return OpenRequest(request)
+
 
 def parameters_string_to_dict(parameters):
     paramDict = {}
@@ -15,12 +49,14 @@ def parameters_string_to_dict(parameters):
     return paramDict
 
 
-def debugLog(message,type):
-    output = "[ORF TVTHEK] ("+type+") "+message;
-    xbmc.log(msg=output, level=xbmc.LOGDEBUG)
+def debugLog(message, loglevel=xbmc.LOGDEBUG):
+    output = "[ORF TVTHEK] " + message
+    xbmc.log(msg=output, level=loglevel)
+
 
 def notifyUser(message):
     addon = xbmcaddon.Addon()
     name = addon.getAddonInfo('name')
     icon = addon.getAddonInfo('icon')
-    xbmc.executebuiltin('Notification(%s, %s, %s, %s)'%(name,message, "", icon))
+    xbmc.executebuiltin('Notification(%s, %s, %s, %s)' % (name, message, "", icon))
+
